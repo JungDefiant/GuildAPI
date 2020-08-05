8fdc821e780f97e1c6cfbe5901a0fcfe3edebe05
Bryant Davis <bryant.davis@hotmail.com>
2020-08-05 15:57:28 -0700

    tests finished

--- GuildAPI/GuildAPITesting/UnitTest1.cs
+++ GuildAPI/GuildAPITesting/UnitTest1.cs
@@ -48,10 +48,11 @@ namespace GuildAPITesting
                 Name = "Legends of Lesley"
             };
             var update = service.Update(gamesDTO);
+            var actual = context.Games.FindAsync(update.Result.Id).Result;
             Assert.Equal(1, context.Games.CountAsync().Result);
             // Assert.IsType<Games>(actual);
             Assert.Equal(1, update.Id);
-            Assert.Equal("Legends of Lesley", update.Result.Name);
+            Assert.Equal("Legends of Lesley", actual.Name);
 
         }
 
@@ -93,13 +94,12 @@ namespace GuildAPITesting
         }
 
         [Fact]
-        public async void CanCreateGuildAndSaveToDatabase()
+        public void CanCreateGuildAndSaveToDatabase()
         {
             DbContextOptions<GuildAPIDbContext> options = new DbContextOptionsBuilder<GuildAPIDbContext>()
                 .UseInMemoryDatabase("CanCreateGuildAndSaveToDatabase")
                 .Options;
             using GuildAPIDbContext context = new GuildAPIDbContext(options);
-            IGames _games;
             GamesService gameService = new GamesService(context);
             GamesDTO gamesDTO = new GamesDTO()
             {
@@ -122,6 +122,127 @@ namespace GuildAPITesting
 
         }
 
+        [Fact]
+        public void CanUpdateGuildAndSaveToDatabase()
+        {
+            DbContextOptions<GuildAPIDbContext> options = new DbContextOptionsBuilder<GuildAPIDbContext>()
+                .UseInMemoryDatabase("CanUpdateGuildAndSaveToDatabase")
+                .Options;
+            using GuildAPIDbContext context = new GuildAPIDbContext(options);
+            GamesService gameService = new GamesService(context);
+            GamesDTO gamesDTO = new GamesDTO()
+            {
+                Name = "Odins Game"
+            };
+            var createdGame = gameService.Create(gamesDTO);
+            GuildService guildService = new GuildService(context, gameService);
+            Assert.Equal(1, context.Games.CountAsync().Result);
+            GuildsDTO guild = new GuildsDTO()
+            {
+                Name = "Odin Slayers"
+            };
+            var creation = guildService.Create(guild);
+            var association = gameService.AddGameGuild(createdGame.Result.Id, creation.Result.Id);
+
+            GuildsDTO guildsDTO = new GuildsDTO()
+            {
+                Id = 1,
+                Name = "I am updated"
+            };
+
+            var update = guildService.Update(guildsDTO).Result;
+            var actual = context.Guilds.FindAsync(update.Id).Result;
+            Assert.Equal(1, context.Guilds.CountAsync().Result);
+            Assert.IsType<Guilds>(actual);
+            Assert.Equal(1, actual.Id);
+            Assert.Equal("I am updated", actual.Name);
+
+        }
+
+        [Fact]
+        public void CanDeleteGuildAndSaveToDatabase()
+        {
+            DbContextOptions<GuildAPIDbContext> options = new DbContextOptionsBuilder<GuildAPIDbContext>()
+                .UseInMemoryDatabase("CanDeleteGuildAndSaveToDatabase")
+                .Options;
+            using GuildAPIDbContext context = new GuildAPIDbContext(options);
+            GamesService gameService = new GamesService(context);
+            GamesDTO gamesDTO = new GamesDTO()
+            {
+                Name = "Odins Game"
+            };
+            var createdGame = gameService.Create(gamesDTO);
+            GuildService guildService = new GuildService(context, gameService);
+            Assert.Equal(1, context.Games.CountAsync().Result);
+            GuildsDTO guild = new GuildsDTO()
+            {
+                Name = "Odin Slayers"
+            };
+            var creation = guildService.Create(guild);
+            var association = gameService.AddGameGuild(createdGame.Result.Id, creation.Result.Id);
+            Assert.Equal(1, context.Guilds.CountAsync().Result);
+            var delete = guildService.Delete(creation.Result.Id);
+            Assert.Equal(0, context.Guilds.CountAsync().Result);
+
+        }
+
+        [Fact]
+        public void CanGetGuildFromDatabase()
+        {
+            DbContextOptions<GuildAPIDbContext> options = new DbContextOptionsBuilder<GuildAPIDbContext>()
+                .UseInMemoryDatabase("CanGetGuildFromDatabase")
+                .Options;
+            using GuildAPIDbContext context = new GuildAPIDbContext(options);
+            GamesService gameService = new GamesService(context);
+            GamesDTO gamesDTO = new GamesDTO()
+            {
+                Name = "Odins Game"
+            };
+            var createdGame = gameService.Create(gamesDTO);
+            GuildService guildService = new GuildService(context, gameService);
+            Assert.Equal(1, context.Games.CountAsync().Result);
+            GuildsDTO guild = new GuildsDTO()
+            {
+                Name = "Odin Slayers"
+            };
+            var creation = guildService.Create(guild);
+            var association = gameService.AddGameGuild(createdGame.Result.Id, creation.Result.Id);
+            var actual = guildService.GetGuild(creation.Result.Id).Result;
+            Assert.Equal(1, context.Guilds.CountAsync().Result);
+            Assert.IsType<GuildsDTO>(actual);
+            Assert.Equal(1, actual.Id);
+            Assert.Equal("Odin Slayers", actual.Name);
+
+        }
+
+        [Fact]
+
+        public void GetterForGame()
+        {
+            GamesDTO gamesDTO = new GamesDTO();
+
+            gamesDTO.Name = "Bade";
+            gamesDTO.Id = 1;
+
+            Assert.Equal("Bade", gamesDTO.Name);
+            Assert.Equal(1, gamesDTO.Id);
+
+
+        }
+
+        [Fact]
+        public void GetterForGuild()
+        {
+            GuildsDTO guildsDTO = new GuildsDTO();
+
+            guildsDTO.Name = "Bryant";
+            guildsDTO.Id = 1;
+
+            Assert.Equal("Bryant", guildsDTO.Name);
+            Assert.Equal(1, guildsDTO.Id);
+
+
+        }
 
     }
 }
